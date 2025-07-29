2d05bcdd7e05ed083c82eaf56eceacffcbf430ed
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-07-29 08:22:31 -0500

    :sparkles: feat: Enhance home page with input fields and message textarea; integrate PrimeNG InputText and Textarea components for improved user interaction

--- bilaterals-client/src/app/pages/home/home.ts
+++ bilaterals-client/src/app/pages/home/home.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, ButtonModule],
+  imports: [RouterLink, ButtonModule, InputTextModule, TextareaModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export default class Home {
-
-}
+export default class Home {}
